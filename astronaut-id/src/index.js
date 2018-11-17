a8850489a026fd9022fd715c007a59d3659dbc74
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-16 18:01:36 -0800

    update astronaut source

--- astronaut-id/src/index.js
+++ astronaut-id/src/index.js
@@ -9,20 +9,52 @@ const ipfsOptions = {
   }
 }
 
-class Austronaut {
-  constructor() {
+function didToAddress(id) {
+  const exploded = id.split(':')
+  if (exploded.length !== 3) {
+    throw new Error('invalid id')
+  }
+  return exploded[2]
+}
+
+function addressToDid(address) {
+  return `did:astro:${address}`
+}
+
+class Identity {
+  constructor (db) {
+    this.db = db
+  }
+
+  get did () {
+    return addressToDid(this.db.address.root)
+  }
+
+  set (key, value) {
+    return this.db.set(key, value)
+  }
+
+  get (key) {
+    return this.db.get(key)
+  }
+}
+
+class Astronaut {
+  constructor () {
     this.ipfs = new IPFS(ipfsOptions)
     this.orbitdb = new OrbitDB(this.ipfs)
   }
 
-  async createId() {
+  async createIdentity () {
     const db = await this.orbitdb.keyvalue(DB_NAME)
-    return this.addressToId(db.address.root)
+    return new Identity(db)
   }
 
-  addressToId(address) {
-    return `did:astro:${address}`
+  async getIdentity (id) {
+    const address = this.didToAddress(id)
+    const db = await this.orbitdb.keyvalue(`/orbitdb/${address}/${DB_NAME}`)
+    return new Identity(db)
   }
 }
 
-module.exports = Austronaut
+module.exports = Astronaut

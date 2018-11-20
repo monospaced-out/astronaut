482a64b80c7f4bc73efb5d2ee5bc80aec650446c
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-20 03:00:58 -0800

    dev gibberish

--- packages/ki/demo/demo.js
+++ packages/ki/demo/demo.js
@@ -5,45 +5,65 @@ const OrbitKistore = require('../../orbit-kistore/src/orbit-kistore')
 const OrbitConnect = require('../../orbit-connect/src/orbit-connect')
 
 const localNode = '/ip4/127.0.0.1/tcp/4003/ws/ipfs/QmTWv5fGvUSFS8K86zxgGRYCEDLJLqGAXa5yjcZKG6weC5'
-const PINNING_ROOM = 'ki'
 
-const kistoreElliptic = new KistoreElliptic()
+async function setup () {
+  const kistoreElliptic = new KistoreElliptic()
 
-let privateKey = window.localStorage.getItem('privateKey')
-if (!privateKey) {
-  privateKey = kistoreElliptic.createKey().getPrivate('hex')
-  window.localStorage.setItem('privateKey', privateKey)
-} else {
-  kistoreElliptic.importPrivateKey(privateKey)
+  let privateKey = window.localStorage.getItem('privateKey')
+  if (!privateKey) {
+    privateKey = kistoreElliptic.createKey().getPrivate('hex')
+    window.localStorage.setItem('privateKey', privateKey)
+  } else {
+    await kistoreElliptic.importPrivateKey(privateKey)
+  }
+
+  const keyAdapters = [ kistoreElliptic ]
+  // const nodes = [ localNode, '/ip4/35.236.30.233/tcp/4003/ws/ipfs/QmPvaeiTbpXSm4nW9vy5b57mdQ4BWqY5REXFu7W6uGMGKS' ]
+  const nodes = [ localNode ]
+
+  const keystore = new OrbitKistore(keyAdapters)
+  const orbitConnect = new OrbitConnect({
+    nodes,
+    orbitdbOptions: { keystore }
+  })
+
+  const ki = new Ki({ orbitConnect, keystore })
+  window.ki = ki
+  window.keystore = keystore
+  window.orbitConnect = orbitConnect
 }
 
-const keyAdapters = [ kistoreElliptic ]
-const nodes = [ localNode ]
-
-const keystore = new OrbitKistore(keyAdapters)
-const orbitConnect = new OrbitConnect({
-  nodes,
-  room: PINNING_ROOM,
-  orbitdbOptions: { keystore }
-})
-const { orbitdb, broadcastDb } = orbitConnect
-const onCreateDb = broadcastDb.bind(orbitConnect)
-
-const ki = new Ki({ orbitdb, onCreateDb, keystore })
-window.ki = ki
-
-async function start () {
+async function getIdentity () {
   let did = window.localStorage.getItem('did')
   let identity
   if (!did) {
-    identity = await ki.createIdentity()
+    identity = await window.ki.createIdentity()
     did = identity.did
     window.localStorage.setItem('did', did)
   } else {
-    identity = await ki.getIdentity(did)
+    identity = await window.ki.getIdentity(did)
   }
+  window.identity = identity
+  return { did, identity }
+}
+
+async function testA () {
+  const { identity } = await getIdentity()
+  console.log('identity', identity)
+  const val = await identity.get('val')
+  console.log(val)
+  // if (!val) {
+  //   console.log('setting')
+  //   const result = await identity.set('val', 'hehe')
+  //   console.log(result)
+  // }
+}
+
+async function start () {
+  const { ki, keystore, orbitConnect } = window
+  const { did, identity } = await getIdentity()
   console.log(did, identity)
-  const kiClaims = new KiClaims({ ki, did, keystore, orbitdb, onCreateDb })
+  const kiClaims = new KiClaims({ ki, did, keystore, orbitConnect })
   window.kiClaims = kiClaims
   const claim = await kiClaims.issueClaim(did, 'SpiritAnimal', 'bobwhite')
   console.log(claim)
@@ -52,4 +72,6 @@ async function start () {
   console.log(claims)
 }
 
+window.testA = testA
 window.start = start
+setup()

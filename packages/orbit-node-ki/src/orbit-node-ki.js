da18ef36066ddb5052427d3098ed3f2d45e23f0e
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-20 23:38:36 -0800

    Rename orbit-node -> orbit-connect-server

--- packages/orbit-node-ki/src/orbit-node-ki.js
+++ packages/orbit-node-ki/src/orbit-node-ki.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const orbitNode = require('../../orbit-node/src/orbit-node')
+const orbitNode = require('../../orbit-connect-server/src/orbit-connect-server')
 
 const ORBITDB_PATH = process.env['ORBITDB_PATH']
 const PINNING_ROOM = 'ki'

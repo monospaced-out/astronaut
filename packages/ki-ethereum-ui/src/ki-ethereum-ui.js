bb1768bc339cdf36da56d9c536e731ca09b1602b
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-29 15:44:09 -0800

    add twitter and github

--- packages/ki-ethereum-ui/src/ki-ethereum-ui.js
+++ packages/ki-ethereum-ui/src/ki-ethereum-ui.js
@@ -19,7 +19,7 @@ app.mount('body')
 function mainView (state, emit) {
   const {
     did,
-    profileAttributes: { name, bio },
+    profileAttributes: { name, bio, twitter, github },
     inputs,
     image,
     loading
@@ -43,6 +43,7 @@ function mainView (state, emit) {
   `
 
   const viewContent = html`
+    <div>
       <header class="tc pv4 pv5-ns">
         <img src=${src} class="br-100 pa1 ba b--black-10 h3 w3" alt="avatar">
         <h1 class="f5 f4-ns fw6 mid-gray">${_name}</h1>
@@ -52,7 +53,34 @@ function mainView (state, emit) {
         ${(bio && bio.value) ? html`
           <h2 class="f5 gray fw2 tracked">${bio.value}</h2>
         ` : null}
+        <ul class="list pl0 measure center">
+          ${twitter && twitter.value ? html`
+            <li class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30 tl">
+              <a class="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://twitter.com/${twitter.value}" target="_blank" title="">
+                <svg data-icon="twitter" viewBox="0 0 32 32" style="fill:currentcolor">
+                  <path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4" />
+                </svg>
+              </a>
+              <a class="link dim gray dib h2" href="https://twitter.com/${twitter.value}" target="_blank" title="">
+                ${twitter.value}
+              </a>
+            </li>
+          ` : null}
+          ${github && github.value ? html`
+            <li class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30 tl">
+              <a class="link dim gray dib br-100 h2 w2 mr3 pa2 bg-near-white ba b--black-10" href="https://github.com/${github.value}" target="_blank" title="">
+                <svg data-icon="github" viewBox="0 0 32 32" style="fill:currentcolor">
+                  <path d="M0 18 C0 12 3 10 3 9 C2.5 7 2.5 4 3 3 C6 3 9 5 10 6 C12 5 14 5 16 5 C18 5 20 5 22 6 C23 5 26 3 29 3 C29.5 4 29.5 7 29 9 C29 10 32 12 32 18 C32 25 30 30 16 30 C2 30 0 25 0 18 M3 20 C3 24 4 28 16 28 C28 28 29 24 29 20 C29 16 28 14 16 14 C4 14 3 16 3 20 M8 21 A1.5 2.5 0 0 0 13 21 A1.5 2.5 0 0 0 8 21 M24 21 A1.5 2.5 0 0 0 19 21 A1.5 2.5 0 0 0 24 21 z" />
+                </svg>
+              </a>
+              <a class="link dim gray dib h2" href="https://github.com/${github.value}" target="_blank" title="">
+                ${github.value}
+              </a>
+            </li>
+          ` : null}
+        </ul>
       </header>
+    </div>
   `
 
   const editContent = html`
@@ -82,6 +110,14 @@ function mainView (state, emit) {
               <div class="mt3">
                 <label class="db fw4 lh-copy f6" for="bio">Bio</label>
                 <textarea id="bio" name="bio" class="pa2 input-reset ba bg-transparent w-100 measure" onchange=${updateInput('bio')}>${inputs.bio || ''}</textarea>
+              </div>
+              <div class="mt3">
+                <label class="db fw4 lh-copy f6" for="twitter">Twitter</label>
+                <input class="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="twitter" onchange=${updateInput('twitter')} value=${inputs.twitter || ''}>
+              </div>
+              <div class="mt3">
+                <label class="db fw4 lh-copy f6" for="Github">Github</label>
+                <input class="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="github" onchange=${updateInput('github')} value=${inputs.github || ''}>
               </div>
             </fieldset>
             <div class="mt3"><button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onclick=${saveChanges}>Save changes</button></div>
@@ -176,8 +212,15 @@ function store (state, emitter) {
   emitter.on('loadProfile', async function () {
     const name = await state.profile.get(state.did, 'name')
     const bio = await state.profile.get(state.did, 'bio')
-    state.profileAttributes = { name, bio }
-    state.inputs = { name: name.value, bio: bio.value }
+    const twitter = await state.profile.get(state.did, 'twitter')
+    const github = await state.profile.get(state.did, 'github')
+    state.profileAttributes = { name, bio, twitter, github }
+    state.inputs = {
+      name: name.value,
+      bio: bio.value,
+      twitter: twitter.value,
+      github: github.value
+    }
     emitter.emit('endLoading')
   })
 

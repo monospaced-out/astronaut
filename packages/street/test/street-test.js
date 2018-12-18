302e256f37d1d81da353f1fe1a554e3bf4d27869
Tyler Yasaka <contact@tyleryasaka.me>
2018-12-18 02:34:16 -0800

    Clean up with beforeEach

--- packages/street/test/street-test.js
+++ packages/street/test/street-test.js
@@ -1,4 +1,4 @@
-/* globals describe, it */
+/* globals describe, it, beforeEach */
 
 const Street = require('../src/street')
 const assert = require('assert')
@@ -19,13 +19,18 @@ class Clock {
 
 describe('street', function () {
   describe('cred', function () {
-    it('should handle single cred', async function () {
-      const clock = new Clock()
-      const street = new Street({
+    let clock, street
+
+    beforeEach(() => {
+      clock = new Clock()
+      street = new Street({
         limit: 1,
         defaultConfidence: 0.5,
         startTime: clock.time()
       })
+    })
+
+    it('should handle single cred', async function () {
       street.setTrust('a', 'b')
       clock.tick()
       street.addCred('b', 'c', clock.time())
@@ -36,12 +41,6 @@ describe('street', function () {
     })
 
     it('should handle self-issued cred', async function () {
-      const clock = new Clock()
-      const street = new Street({
-        limit: 1,
-        defaultConfidence: 0.5,
-        startTime: clock.time()
-      })
       clock.tick()
       street.addCred('a', 'b', clock.time())
       const { confidence, votes, value } = street.cred('a', 'b', clock.time())
@@ -51,12 +50,6 @@ describe('street', function () {
     })
 
     it('should handle complex cred', async function () {
-      const clock = new Clock()
-      const street = new Street({
-        limit: 1,
-        defaultConfidence: 0.5,
-        startTime: clock.time()
-      })
       street.setTrust('a', 'b')
       street.setTrust('a', 'c')
       street.setTrust('b', 'd')
@@ -73,12 +66,6 @@ describe('street', function () {
     })
 
     it('should only use cred issued after a peer has been trusted', async function () {
-      const clock = new Clock()
-      const street = new Street({
-        limit: 1,
-        defaultConfidence: 0.5,
-        startTime: clock.time()
-      })
       street.addCred('b', 'c', clock.time()) // this one should be ignored
       clock.tick()
       street.setTrust('a', 'b')

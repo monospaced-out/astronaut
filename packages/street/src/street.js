36bea578c2eaff72fba57d021cb45f46fa4b1a10
Tyler Yasaka <contact@tyleryasaka.me>
2018-12-18 01:54:58 -0800

    Differentiate between atheists and agnostics
    An atheist says "there is no god", while an agnostic simply does not say "there is a god". These are two very different things. It applies here because I was previously treating the statement, "peer A has cred value of 0" the same as "I have nothing to say of peer A", which meant that going from not saying anything to saying just a little bit could produce unexpected results.

--- packages/street/src/street.js
+++ packages/street/src/street.js
@@ -1,8 +1,10 @@
+const Big = require('big.js')
 const P2PTrust = require('../../p2p-trust/src/p2p-trust')
 const jacobsMetric = require('../../jacobs-metric/src/jacobs-metric')
 
 const CRED_CLAIM = 'cred'
 const TRUST_CLAIM = 'trust'
+const ZERO = new Big(0)
 
 class Street {
   constructor ({ limit, defaultConfidence }) {
@@ -51,7 +53,7 @@ class Street {
           }
           const { confidence, time } = claim
           if (time <= currentTime) {
-            return { value: 1, confidence, to: t }
+            return { value: null, confidence, to: t }
           }
         }).filter(c => c)
       } else if (claimType === CRED_CLAIM) {
@@ -84,13 +86,52 @@ class Street {
           const cumulativeRating = validatedList.reduce((acc, { isNegative }) => {
             return isNegative ? acc - 1 : acc + 1
           }, 0)
-          return { value: cumulativeRating, confidence: 1, to: t }
+          const value = {
+            votes: new Big(validatedList.length),
+            value: new Big(cumulativeRating)
+          }
+          return { value, confidence: 1, to: t }
         }).filter(c => c)
       } else {
         return []
       }
     }
-    const p2pTrust = new P2PTrust({ getClaims, metric: jacobsMetric })
+
+    const getValue = (claims) => {
+      claims = claims.filter(c => c.value)
+
+      // calculate mean value
+      const weights = claims.map(({ confidence, value: { votes } }) => {
+        return confidence.times(votes)
+      })
+      const valueWeightedSum = claims.reduce((acc, { confidence, value: { value, votes } }, i) => {
+        return acc.plus(weights[i].times(value))
+      }, ZERO)
+      const weightSum = weights.reduce((acc, weight) => {
+        return acc.plus(weight)
+      }, ZERO)
+      const meanWeightedValue = weightSum.eq(ZERO)
+        ? ZERO
+        : valueWeightedSum.div(weightSum)
+
+      // calculate mean votes
+      const confidenceSum = claims.reduce((acc, { confidence }) => {
+        return acc.plus(confidence)
+      }, ZERO)
+      const votesWeightedSum = claims.reduce((acc, { confidence, value: { votes } }) => {
+        return acc.plus(confidence.times(votes))
+      }, ZERO)
+      const meanWeightedVotes = votesWeightedSum.eq(ZERO)
+        ? ZERO
+        : votesWeightedSum.div(confidenceSum)
+
+      return {
+        value: meanWeightedValue,
+        votes: meanWeightedVotes
+      }
+    }
+
+    const p2pTrust = new P2PTrust({ getClaims, getValue, metric: jacobsMetric })
     return p2pTrust.claimConfidence(from, to, CRED_CLAIM)
   }
 }

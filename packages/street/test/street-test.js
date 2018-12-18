a2e9c6ab77a336fc32e6d81fc28cd6a7097a48e4
Tyler Yasaka <contact@tyleryasaka.me>
2018-12-17 16:33:52 -0800

    add test

--- packages/street/test/street-test.js
+++ packages/street/test/street-test.js
@@ -47,5 +47,26 @@ describe('street', function () {
       assert.strictEqual(confidence.toString(), '1')
       assert.strictEqual(value.toString(), '1')
     })
+
+    it('should handle complex cred', async function () {
+      const clock = new Clock()
+      const street = new Street({
+        limit: 1,
+        getTime: clock.time.bind(clock),
+        defaultConfidence: 0.5
+      })
+      street.setTrust('a', 'b', clock.time())
+      street.setTrust('a', 'c', clock.time())
+      street.setTrust('b', 'd', clock.time())
+      street.setTrust('c', 'd', clock.time())
+      clock.tick()
+      street.addCred('b', 'e', clock.time())
+      street.addCred('d', 'e', clock.time())
+      clock.tick()
+      street.addCred('d', 'e', clock.time())
+      const { confidence, value } = street.cred('a', 'e')
+      assert.strictEqual(confidence.toString(), '0.625')
+      assert.strictEqual(value.toString(), '1.55555555555555555555')
+    })
   })
 })

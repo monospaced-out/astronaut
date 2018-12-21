70ea6b73ba4246093939db3e25a2dfa19ca5ce48
Tyler Yasaka <contact@tyleryasaka.me>
2018-12-21 01:46:44 -0800

    more experimentation

--- packages/street/simulations/beta.js
+++ packages/street/simulations/beta.js
@@ -23,9 +23,9 @@ class Clock {
 const n = new Big(50) // total number of nodes
 const k = new Big(4) // number of connections per node
 const m = new Big(0.5) // probability of a node being malicious
-const time = 500
-const confidence = new Big(0.1) // confidence to place in each trust claim
-const accuracy = new Big(0.1) // probability that each trust link is not mistakenly pointed to a malicious node
+const time = 1
+const confidence = new Big(0.5) // confidence to place in each trust claim
+const accuracy = new Big(0.5) // probability that each trust link is not mistakenly pointed to a malicious node
 // const beta = 0
 
 function connect (a, b, street) {
@@ -74,7 +74,7 @@ function run () {
       const rand = new Big(Math.random())
       const randomIndex = Math.floor(Number(rand.times(n).toString()))
       const randomNode = nodes[randomIndex]
-      if ((node.isMalicious && !randomNode.isMalicious) || (!node.isMalicious && randomNode.isMalicious)) {
+      if ((node.isMalicious !== randomNode.isMalicious)) {
         street.removeCred(String(node.name), String(randomNode.name), clock.time())
       } else {
         street.addCred(String(node.name), String(randomNode.name), clock.time())
@@ -93,8 +93,8 @@ function run () {
     nodes.forEach(to => {
       const cred = street.cred(String(from.name), String(to.name), 0, clock.time())
       const correctness = to.isMalicious
-        ? cred.value.times(NEGATIVE_ONE).round(5)
-        : cred.value.round(5)
+        ? cred.value.times(cred.confidence).times(NEGATIVE_ONE).round(5)
+        : cred.value.times(cred.confidence).round(5)
       resultMatrix[from.name][to.name] = {
         from: from.name,
         to: to.name,
@@ -111,11 +111,36 @@ function run () {
 
   console.log('analyzing...')
 
-  const correctnesses = flattened.map(({ correctness }) => Number(correctness))
-  const incorrectnesses = flattened.map(({ incorrectness }) => Number(incorrectness))
-  const meanCorrectness = ss.mean(correctnesses)
-  const medianCorrectness = ss.median(correctnesses)
-  const correctQuantile = ss.quantileRank(incorrectnesses, 0)
+  const getCorrectness = (arr) => {
+    return arr.map(({ correctness }) => {
+      return Number(correctness)
+    })
+  }
+
+  const getIncorrectness = (arr) => {
+    return arr.map(({ incorrectness }) => {
+      return Number(incorrectness)
+    })
+  }
+
+  const good = flattened.filter(({ isMalicious }) => !isMalicious)
+  const bad = flattened.filter(({ isMalicious }) => isMalicious)
+  const goodCorrect = good.filter(({ correctness }) => correctness > 0)
+  const goodIncorrect = good.filter(({ correctness }) => correctness < 0)
+  const badCorrect = bad.filter(({ correctness }) => correctness > 0)
+  const badIncorrect = bad.filter(({ correctness }) => correctness < 0)
+  const goodMean = ss.mean(getCorrectness(good))
+  const goodMedian = ss.median(getCorrectness(good))
+  const goodVariance = ss.variance(getCorrectness(good))
+  const goodQuantile = ss.quantile(getIncorrectness(good), 0.95) * -1
+  const goodQuantileRank = ss.quantileRank(getIncorrectness(good), 0)
+  const badMean = ss.mean(getCorrectness(bad))
+  const badMedian = ss.median(getCorrectness(bad))
+  const badVariance = ss.variance(getCorrectness(bad))
+  const badQuantile = ss.quantile(getCorrectness(bad), 0.95)
+  const badQuantileRank = ss.quantileRank(getCorrectness(bad), 0)
+  const failureRate = (badIncorrect.length + goodIncorrect.length) / flattened.length
+  const successRate = (badCorrect.length + goodCorrect.length) / flattened.length
 
   const half = Math.round(flattened.length / 2)
   const bayes = new ss.BayesianClassifier()
@@ -124,9 +149,18 @@ function run () {
   })
 
   const stats = {
-    meanCorrectness,
-    medianCorrectness,
-    correctQuantile
+    goodMean,
+    goodMedian,
+    goodVariance,
+    goodQuantile,
+    goodQuantileRank,
+    badMean,
+    badMedian,
+    badVariance,
+    badQuantile,
+    badQuantileRank,
+    successRate,
+    failureRate
   }
 
   console.log('done')
